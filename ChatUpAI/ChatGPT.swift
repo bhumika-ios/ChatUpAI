4ecd730dda59cb8d704b495c53e921f6173c21c6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-05-04 12:52:30 +0530

    create ChatGPT file for login and register purpose

---
+++ ChatUpAI/ChatGPT.swift
@@ -0,0 +1,20 @@
+//
+//  ChatGPT.swift
+//  ChatUpAI
+//
+//  Created by Bhumika Patel on 04/05/23.
+//
+
+import SwiftUI
+
+struct ChatGPT: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatGPT_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatGPT()
+    }
+}
