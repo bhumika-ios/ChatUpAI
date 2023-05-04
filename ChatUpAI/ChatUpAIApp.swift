9f1e49dec87f33cc18968fe2d28d2fc2b173135b
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-05-04 12:44:22 +0530

    Initial Commit

---
+++ ChatUpAI/ChatUpAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUpAIApp.swift
+//  ChatUpAI
+//
+//  Created by Bhumika Patel on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatUpAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
