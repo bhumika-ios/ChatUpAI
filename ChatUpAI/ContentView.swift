f02154768eb619792d9bf5167b48c39a43f4c25b
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-05-13 16:43:23 +0530

    call file gptpage for login and signup in content view

--- ChatUpAI/ContentView.swift
+++ ChatUpAI/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ChatGPTPage()
         }
-        .padding()
     }
 }
 
@@ -24,3 +20,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
